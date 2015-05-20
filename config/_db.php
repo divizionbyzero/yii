4e1eeee9861dbb5ada5c3fdb8e505ad02e3ae950
divizionbyzero <divizionbyzero@bk.ru>
2015-05-20 21:54:25 +0300

    css fix 1

--- config/_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii_blog',
-    'username' => 'root',
-    'password' => 'qweqweqwe',
-    'charset' => 'utf8',
-];
---
+++ web/js/hideside.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hele on 20.05.2015.
+ */
+if (document.querySelector(".site-index")!=null) {
+    document.querySelector(".sidebar").setAttribute("style","display:none");
+    document.querySelector(".content").setAttribute("style","margin-right:0")
+}
